4e2d706c904fbd063faf41dbd9ba0bff5be00b8f
MosharrofHossain44 <mdmosharrof44677@gmail.com>
2025-01-13 11:18:32 +0600

    Categories functionality

--- JS/test.js
+++ JS/test.js
@@ -1,10 +1,10 @@
-function getTimeString(second){
-    const hour = parseInt(second/3600)
-    const minute = parseInt((second%3600)/60)
-    const remainingSecond = ((second%3600)%60)
+function getTimeString(second) {
+    const hour = parseInt(second / 3600)
+    const minute = parseInt((second % 3600) / 60)
+    const remainingSecond = ((second % 3600) % 60)
 
-    return `${hour}hrs ${minute}mnt ${remainingSecond}scnd`
-    
+    return `${hour}hrs ${minute}mnt ${remainingSecond}scn`
+
 }
 
 
--- JS/video.js
+++ JS/video.js
@@ -14,16 +14,29 @@ const loadVideos = () => {
 }
 
 //load categories video
-const loadCategoriesVideos = (id) =>{
+const loadCategoriesVideos = (id) => {
     // alert(id)
     fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
         .then(res => res.json())
-        .then(data => displayVideos(data.category))
+        .then(data => {
+            removeActiveClass()
+            const activeBtn = document.getElementById(`btn-${id}`)
+            activeBtn.classList.add("active")
+
+            displayVideos(data.category)
+        })
         .catch((err) => console.log(err))
 
 }
 
 
+//remove active class
+const removeActiveClass = () => {
+    const getAllActiveBtn = document.getElementsByClassName('removeId')
+    for(btn of getAllActiveBtn){
+        btn.classList.remove('active')
+    }
+}
 
 
 //display categories
@@ -36,7 +49,7 @@ const displayCategories = (categories) => {
         const btnContainer = document.createElement('div')
         btnContainer.innerHTML =
             `
-            <button onclick="loadCategoriesVideos(${item.category_id})" class="btn">
+            <button id="btn-${item.category_id}" onclick="loadCategoriesVideos(${item.category_id})" class="btn removeId">
             ${item.category}
             </button>
         `
@@ -49,76 +62,76 @@ const displayCategories = (categories) => {
 
 
 
-    //     {
-    //       "category_id": "1001",
-    //       "video_id": "aaaa",
-    //       "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-    //       "title": "Shape of You",
-    //       "authors": [
-    //         {
-    //           "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-    //           "profile_name": "Olivia Mitchell",
-    //           "verified": ""
-    //         }
-    //       ],
-    //       "others": {
-    //         "views": "100K",
-    //         "posted_date": "16278"
-    //       },
-    //       "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-    //     }
+//     {
+//       "category_id": "1001",
+//       "video_id": "aaaa",
+//       "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//       "title": "Shape of You",
+//       "authors": [
+//         {
+//           "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//           "profile_name": "Olivia Mitchell",
+//           "verified": ""
+//         }
+//       ],
+//       "others": {
+//         "views": "100K",
+//         "posted_date": "16278"
+//       },
+//       "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+//     }
 
 
 
-    // getTimeString from second
-    function getTimeString(second) {
-        const hour = parseInt(second / 3600)
-        const minute = parseInt((second % 3600) / 60)
-        const remainingSecond = ((second % 3600) % 60)
+// getTimeString from second
+function getTimeString(second) {
+    const hour = parseInt(second / 3600)
+    const minute = parseInt((second % 3600) / 60)
+    const remainingSecond = ((second % 3600) % 60)
 
-        return `${hour}hrs ${minute}mnt ${remainingSecond}scn`
+    return `${hour}hrs ${minute}mnt ${remainingSecond}scn`
 
-    }
-
-
+}
 
 
-    //display videos
-    const displayVideos = (videos) => {
-        const videoContainer = document.getElementById('videoContainer')
-        videoContainer.innerHTML = ""
 
-        if(videos.length == 0){
-            videoContainer.classList.remove("grid")
-            videoContainer.innerHTML = 
+
+//display videos
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('videoContainer')
+    videoContainer.innerHTML = ""
+
+    if (videos.length == 0) {
+        videoContainer.classList.remove("grid")
+        videoContainer.innerHTML =
             `
                 <div class="min-h-[300px] flex flex-col justify-center items-center">
                     <img class="" src="assets/Icon.png"/>
                     <h2 class="text-center mt-6 text-xl font-bold">
-                    Ops Sorry!<br>
-                    No content in this category
+                    Oops! Sorry.<br>
+                    No content in this category.
                 </h2>
                 </div>
                 
             `
-        }
-        else{
-            videoContainer.classList.add("grid")
-        }
+    }
+    else {
+        videoContainer.classList.add("grid")
+    }
 
 
-        videos.forEach((video) => {
-            // console.log(video)
-            const videoCard = document.createElement('div')
-            videoCard.classList = "card card-compact"
+    videos.forEach((video) => {
+        // console.log(video)
+        const videoCard = document.createElement('div')
+        videoCard.classList = "card card-compact"
 
-            videoCard.innerHTML = `
+        videoCard.innerHTML = `
         <figure class= "h-[200px] relative">
         <img class="h-full w-full object-cover"
         src= ${video.thumbnail}
         alt="Shoes" />
         ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-2 bg-black px-2 py-1 rounded text-white text-xs">${getTimeString(video.others.posted_date)}</span>`
-                }
+            }
         </figure>
         
         <div class="px-0 py-2 flex gap-2">
@@ -138,10 +151,10 @@ const displayCategories = (categories) => {
 
 
 
-            videoContainer.appendChild(videoCard)
-        })
-    }
+        videoContainer.appendChild(videoCard)
+    })
+}
 
 
-    loadCategories()
-    loadVideos()
+loadCategories()
+loadVideos()
